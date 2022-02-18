e71fa042a5fe34fb20dfab892b33a3ecb85ff4c8
bhavesh42 <bajajb@rpi.edu>
2022-02-18 12:42:37 -0500

    Angular App + TypeScript

--- app.ts
+++ app.ts
@@ -8,24 +8,33 @@ interface initialize {
 abstract class abstract_game { // abstract class
   abstract target_color: [number, number, number];
   abstract input_color: [number, number, number];
-  public name: string;
-  public time: number;
-
-  constructor(init: initialize) { // interface
-    if(init.time) this.time = init.time;
-    else this.time = 60;
-
-    this.name = init.name;
-  }
+  abstract name: string;
+  abstract time: number;
 
   abstract generateColor(): void;
   abstract generateColor(color: [number, number, number]): void; // overloading
 
   abstract slider(value: number, color_type: number): void;
+
+  abstract calculate_score(seconds_remaining: number): number;
 }
 
 class game extends abstract_game { // normal class
-  
+  public target_color: [number, number, number];
+  public input_color: [number, number, number];
+  public name: string;
+  public time: number;
+
+  constructor(init: initialize) { // interface
+    super();
+    if(init.time) {
+      if(init.time < 1 || init.time > 100) this.time = 60;
+      else this.time = init.time;
+    }
+    else this.time = 60;
+
+    this.name = init.name;
+  }
 
   // constructor(init: initialize, initial_color: [number, number, number]) { // interface
   //   super();
@@ -35,27 +44,29 @@ class game extends abstract_game { // normal class
   //   this.name = init.name;
   // }
 
-  generateColor(): void {
+  generateColor(color?: [number, number, number]): void {
+    if(color) {
+      this.target_color = color;
+      return;
+    }
     this.target_color[0] = Math.floor(Math.random() * 255);
     this.target_color[1] = Math.floor(Math.random() * 255);
     this.target_color[2] = Math.floor(Math.random() * 255);
   }
 
-  generateColor(color: [number, number, number]): void {
-    this.target_color = color;
-  }
-
   slider(value: number, color_type: number): void {
     this.input_color[color_type] = value;
   }
-}
 
-function calculate_score(){
-//(255 - abs(actual_red - guessed_red)) + 
-//(255 - abs(actual_green - guessed_green)) + 
-//(255 - abs(actual_blue - gussed_blue)) * floor(seconds_remaining) * 
-//(1000 * (101 - seconds_selected))
+  calculate_score(seconds_remaining: number): number {
+    let actual_red = this.target_color[0];
+    let guessed_red = this.input_color[0];
+    let actual_green = this.target_color[1];
+    let guessed_green = this.input_color[1];
+    let actual_blue = this.target_color[2];
+    let guessed_blue = this.target_color[2];
 
-
-
+    return (255 - Math.abs(actual_red - guessed_red)) + (255 - Math.abs(actual_green - guessed_green))
+      + (255 - Math.abs(actual_blue - guessed_blue)) * Math.floor(seconds_remaining) * (1000 * (101 - this.time));
+  }
 }
