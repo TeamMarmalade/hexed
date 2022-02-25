f1bfb04d34d9d6e405b4670fd2e338ac49b2188b
bhavesh42 <bajajb@rpi.edu>
2022-02-24 22:54:57 -0500

    updated timer input

--- hexed/src/app/app.component.ts
+++ hexed/src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'hexed';
   color: [number, number, number] = [0,0,0];
   score: number = 0;
-  timer: number = 0;
+  timer: number = 60;
 
   changeValue(c: number, value: number) {
     this.color[c] = value;
