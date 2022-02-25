4cacf8f9e65646c4da342467f32787ce27d4e5ad
Cosillow <silloc@rpi.edu>
2022-02-25 02:35:18 -0500

    completed intialization of the game

--- hexed/src/app/app.component.ts
+++ hexed/src/app/app.component.ts
@@ -9,9 +9,16 @@ export class AppComponent {
   title = 'hexed';
   color: [number, number, number] = [0,0,0];
   score: number = 0;
-  timer: number = 60;
+  time: number = 60;
+  gameStart: boolean = false;
+  name: string = '';
 
-  changeValue(c: number, value: number) {
+  changeValue(c: number, value: number) { // slider component
     this.color[c] = value;
+  }
+  saveInit(Init: [string, number]) { // name component
+    this.time = Init[1];
+    this.name = Init[0];
+    this.gameStart = true;
   }
 }
--- hexed/src/app/name/name.component.ts
+++ hexed/src/app/name/name.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-name',
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./name.component.css']
 })
 export class NameComponent implements OnInit {
+  // send name component to parent
+  @Output() NameEmitter = new EventEmitter<[string, number]>();
+  saveInit(init: [string, number]) {
+    if (init[0] != '') { // user must enter a name
+      let time: number = 60;
+      if (init[1] >= 1 && init[1] <= 100) {
+        time = init[1];
+      }
+      this.NameEmitter.emit([init[0], time]);
+    }
+  }
 
   constructor() { }
 
