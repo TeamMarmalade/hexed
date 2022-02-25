96b4889fd7a460b39793ecb708e296ebe5254112
bhavesh42 <bajajb@rpi.edu>
2022-02-25 13:14:24 -0500

    Score change

--- hexed/src/app/app.component.ts
+++ hexed/src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
   score: number = 0;
   formShow: boolean = false;
   name: string = '';
-  timer: number = 0;
+  // timer: number = 0;
   random_color: [number, number, number] = [0, 0, 0];
   game_start: boolean = false;
   win: boolean = false;
@@ -44,16 +44,36 @@ export class AppComponent {
     if(this.color[0] === this.random_color[0] && this.color[1] === this.random_color[1] && this.color[2] === this.random_color[2]) {
       this.win = true;
       this.game_start = false;
+      this.score = ((255 - Math.abs(this.random_color[0] - this.color[0])) + (255 - Math.abs(this.random_color[1] - this.color[1])) + (255 - Math.abs(this.random_color[2] - this.color[2]))) * Math.floor(this.time) * (1000 * (101 - this.original_time));
+
       // this.time will give current time
-      alert(`Correct: Completed in ${this.original_time - this.time} seconds`);
+      // alert(`Correct: Completed in ${this.original_time - this.time} seconds`);
+      alert(`You won! Your score: ${this.score}`);
     }
     else {
-      alert("Not Correct");
+      this.win = false;
+      this.game_start = false;
+      this.score = ((255 - Math.abs(this.random_color[0] - this.color[0])) + (255 - Math.abs(this.random_color[1] - this.color[1])) + (255 - Math.abs(this.random_color[2] - this.color[2]))) * Math.floor(this.time) * (1000 * (101 - this.original_time));
+
+      // this.time will give current time
+      // alert(`Correct: Completed in ${this.original_time - this.time} seconds`);
+      alert(`You lose. Your score: ${this.score}`);
     }
   }
+  /*
+  ((255 - abs(actual_red - guessed_red))
+  + (255 - abs(actual_green - guessed_green))
+  + (255 - abs(actual_blue - gussed_blue))) * floor(seconds_remaining)
+  * (1000 * (101 - seconds_selected))
+  */
 
   failure(value: boolean): void {
-    if(!value) alert("Failed to guess color in time");
+    console.log(this.random_color);
+    console.log(this.color);
+
+    this.score = ((255 - Math.abs(this.random_color[0] - this.color[0])) + (255 - Math.abs(this.random_color[1] - this.color[1])) + (255 - Math.abs(this.random_color[2] - this.color[2]))) * Math.floor(this.time) * (1000 * (101 - this.original_time));
+    console.log(this.score);
+    alert(`You lost. Your score: ${this.score}`);
     this.formShow = false;
   }
 
