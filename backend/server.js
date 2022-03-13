97438e6797cc648c39e6d445317a526bc7e2eca3
Cosillow <silloc@rpi.edu>
2022-03-13 00:40:49 -0500

    endpoints created for external high scores

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,8 @@ app.get('/getscores', (req, res) => {
   // return universal player high scores
   // https://freebee.fun/cgi-bin/scores
   let api_url = "https://freebee.fun/cgi-bin/scores";
-  axios.request(api_url).then((res) => {
-    console.log(res["data"]);
-    let js = JSON.parse(res['data'][0]);
-    console.log(js);
-    res.status(200).json(js);
+  axios.request(api_url).then((scores) => {
+    res.status(200).json(scores['data']);
   }).catch((err) => {
     console.log(err);
     res.status(500).send();
@@ -28,11 +25,45 @@ app.get('/getscores', (req, res) => {
 app.get('/sendscore', (req, res) => {
   // insert a new score into the high scores
   // https://freebee.fun/cgi-bin/scores?<name>=<score>
-  axios.request(api_url).then((res) => {
 
-  }).catch((err) => {
-
-  });
+  // request body should be valid JSON
+  let js = req.body;
+  try {
+    // make sure it's JSON
+    JSON.stringify(js); // may throw error
+    if (req.body.hasOwnProperty('name') && req.body.hasOwnProperty('score') && Object.keys(req.body).length == 2) {
+      let name = String(req.body.name);
+      let score= String(req.body.score);
+      let api_url = `https://freebee.fun/cgi-bin/scores?${name}=${score}`;
+      console.log(api_url);
+      axios.request(api_url).then((notif) => {
+        notif = notif["data"];
+        if (notif.hasOwnProperty("update") && notif.update == "success") {
+          res.status(200).json(notif);
+          return;
+        } else {
+          // something wrong on professor's api I believe
+          res.status(500).json(notif);
+          return;
+        }
+      }).catch((err) => {
+        res.status(500).send();
+        return;
+      });
+    } else {
+      // invalid json object in request body
+      res.status(400).send();
+      return
+    }
+    
+  } catch(e) {
+    console.log(e);
+    // its not JSON
+    res.send(400);
+    return;
+    // bad request body
+  }
+  
 });
 
 app.listen(port, () => {
